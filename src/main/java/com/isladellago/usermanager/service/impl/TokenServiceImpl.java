ee65f10b4cafbaa44153ee9174fe27f887483b11
Johan Camilo Lopez <camilino123@gmail.com>
2021-10-01 10:04:18 -0500

    Service structure created and JWT dependency added

---
+++ src/main/java/com/isladellago/usermanager/service/impl/TokenServiceImpl.java
@@ -0,0 +1,12 @@
+package com.isladellago.usermanager.service.impl;
+
+import com.isladellago.usermanager.dto.TokenDto;
+import com.isladellago.usermanager.service.TokenService;
+
+public class TokenServiceImpl implements TokenService {
+
+    @Override
+    public TokenDto generateToken() {
+        return null;
+    }
+}
